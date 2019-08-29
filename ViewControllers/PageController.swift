8fc0dfeaf6169dc49058e7a3ce6cb3c2701a06ed
Maheep Chhabra <rahul129.sharma@gmail.com>
2019-08-29 15:52:27 +0530

    setup story model

---
+++ ViewControllers/PageController.swift
@@ -0,0 +1,34 @@
+//
+//  PageController.swift
+//  InteractiveGameSelf
+//
+//  Created by Maheep on 29/08/19.
+//  Copyright © 2019 splashTeam. All rights reserved.
+//
+
+import UIKit
+
+class PageController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    class PageController {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
